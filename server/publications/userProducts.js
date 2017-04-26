b589af5c834ea2d51e214fd28f6b700d215fac55
Michaela <michnich07@gmail.com>
2017-04-26 14:11:29 -0400

    removed userproducts collection and publication, now in product upload plugin

--- server/publications/userProducts.js
+++
@@ -1,6 +0,0 @@
-import { Mongo } from "meteor/mongo";
-import { UserProducts } from "/lib/collections";
-
-Meteor.publish('userProductsByUser', function(userId) {
-	return UserProducts.find({"userId": userId});
-});
